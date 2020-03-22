e8eda69118902375e463f4c9344fd5e05cde6bc4
王玺凯 <XIXI@>
2020-03-23 00:16:40 +0800

    组长完善了布局页

---
+++ EcommerceFromUI/EcommerceFrom/Controllers/commodityController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ApiSDKClient;
+using BLL;
+using BLL.APIRequest;
+namespace EcommerceFrom.Controllers
+{
+    public class CommodityController : Controller
+    {
+        // GET: commodity
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
